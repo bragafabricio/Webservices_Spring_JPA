4e61a014a15f35c2dc2799b4ac216775d2ce040b
Fabricio Braga <fabriciobrg@gmail.com>
2021-09-23 17:39:30 -0300

    User delete

--- src/main/java/com/projeto/demo/services/UserService.java
+++ src/main/java/com/projeto/demo/services/UserService.java
@@ -5,6 +5,7 @@ import com.projeto.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,5 +27,9 @@ public class UserService {
 
     public User insert(User obj){
         return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
     }
 }
